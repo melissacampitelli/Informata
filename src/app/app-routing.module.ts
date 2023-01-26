de2ce9ff0223fce83d3080492e9f86fc5583d6d1
melissacampitelli <melissamatos2001@hotmail.com>
2023-01-26 19:53:44 -0300

    :art: Routing utilizado
    routing pronto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
-import { NgModule } from '@angular/core';
+import {  NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TelaCadastroComponent } from './tela-cadastro/tela-cadastro.component';
+import { TelaLoginComponent } from './tela-login/tela-login.component';
+import { TelaProdutosComponent } from './tela-produtos/tela-produtos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'cadastro', component: TelaCadastroComponent },
+  { path:'login', component: TelaLoginComponent },
+  { path:'produtos', component: TelaProdutosComponent }
+];
 
 @NgModule({
+  declarations:[],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
