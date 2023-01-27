ea2c35ade8dae4dc411eb2301673e39de8f17675
melissacampitelli <melissamatos2001@hotmail.com>
2023-01-27 11:43:03 -0300

    :art: Update CSS
    CSS modificado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TelaLoginComponent } from './tela-login/tela-login.component';
 import { TelaCadastroComponent } from './tela-cadastro/tela-cadastro.component';
 import { TelaProdutosComponent } from './tela-produtos/tela-produtos.component';
+import {FormsModule ,ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,10 @@ import { TelaProdutosComponent } from './tela-produtos/tela-produtos.component';
     TelaProdutosComponent
   ],
   imports: [
+    ReactiveFormsModule,
     BrowserModule,
+    CommonModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
